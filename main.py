ed3da35480a604fcad33fe95a693fab4e4f76176
Yasmin Karolina <140443855+Yasmink-godinho@users.noreply.github.com>
2025-11-01 10:53:54 -0300

    criação do banco de dados

---
+++ main.py
@@ -0,0 +1,8 @@
+from database import start_data_bases # 1. Importa a sua função do arquivo database.py
+# (A Pessoa 2 da sua equipe irá adicionar o 'from menu import ...' aqui depois)
+
+if __name__ == "__main__":
+    print("Iniciando o sistema...")
+
+    # 2. Executa a sua função para criar o banco de dados e as tabelas
+    start_data_bases()
